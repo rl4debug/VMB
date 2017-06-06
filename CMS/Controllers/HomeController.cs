953f227dc208408a4f51063f64b61c5f5c397aaa
tvb <rl.4.debug@gmail.com>
2017-06-07 02:37:34 +0700

    aaaa

---
+++ CMS/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace CMS.Controllers
+{
+    [SessionState(System.Web.SessionState.SessionStateBehavior.ReadOnly)]
+    public class HomeController : Controller
+    {
+        //public ActionResult Index()
+        //{
+        //    var mvcName = typeof(Controller).Assembly.GetName();
+        //    var isMono = Type.GetType("Mono.Runtime") != null;
+
+        //    ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+        //    ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+
+        //    return View();
+        //}
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
